d141566f6c6923513e83fea7de4afcf5c5fd4aa1
sianour <siavash.nouri1368@gmail.com>
2021-01-18 17:09:32 +0330

    siavash

---
+++ 4/forth session of web 3.js
@@ -0,0 +1,12 @@
+//Do fibonacci function without recursive function
+
+function fibonacci(x) {
+    let arr = [0, 1, 1];
+    // in fibonacci first 3 element is static number
+    for (let i = 3; i < x + 1; i++) {
+        arr.push(arr[i - 2] + arr[i - 1])
+    }
+    return arr[x]
+}
+console.log(fibonacci(8))
+© 2021 GitHub, Inc.
